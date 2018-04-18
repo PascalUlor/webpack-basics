5c10b84afd9909bdb7a6afa6f8cb6e53307216f2
Pascal Ulor <pascalulor@yahoo.com>
2018-04-18 01:53:53 +0100

    Setup webpack to bundle scss html files and imgaes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,62 @@
 let path = require('path');
-let webpack = require('webpack');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+
+let extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+})
 
 module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/dist'
+        // publicPath: '/dist'
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.js$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: [ 'css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
                 ]
             }
         ]
     },
-    // plugins: [
-    //     new webpack.optimize.UglifyJsPlugin({
-    //         // ...
-    //     })
-    // ]
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
 };
